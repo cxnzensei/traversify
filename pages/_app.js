0d17edbb22c4dcf71db52139befd2bbe919a658b
cxnzensei <nishitpatil3231@gmail.com>
2022-06-23 08:25:21 +0530

    updated _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,6 @@ import '../styles/globals.css'
 import Layout from '../components/Layout'
 import ProgressBar from '@badrap/bar-of-progress'
 import Router from 'next/router'
-import { useEffect, useState } from 'react'
 
 const progress = new ProgressBar({
   size: 4,
@@ -17,19 +16,6 @@ Router.events.on('routeChangeError', progress.finish)
 
 
 function MyApp({ Component, pageProps }) {
-
-  const [token, setToken] = useState(null)
-
-  useEffect(() => {
-    setToken(localStorage.getItem('token'));
-
-    if(token !== null) {
-      if(Router.pathname === '/login' || Router.pathname === '/signup') {
-        Router.push('/')
-      }
-    }
-  }, [token])
-
   return (
     <Layout>
       <Component {...pageProps} />
