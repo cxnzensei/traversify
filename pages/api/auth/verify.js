bf03291bf60feea5d2a1ab0b2ebab9ae6462eee0
cxnzensei <nishitpatil3231@gmail.com>
2022-06-23 08:29:00 +0530

    updated api responses on get request

--- pages/api/auth/verify.js
+++ pages/api/auth/verify.js
@@ -1,13 +1,16 @@
 const pool = require('../../../postgresql/db')
-import authorization from '../../../middlewares/authorization'
 
 async function verify(req, res) {
     const { method, cookies } = req
 
     if(method === 'GET') {
         try {
-            const user = await pool.query('select user_id, house, firstname, lastname, username, email from users left join houses using(house_id) where user_id = $1', [cookies.user])
-            res.json({ "user": user.rows[0], "auth": true })
+            if(cookies.token) {
+                const user = await pool.query('select user_id, house, firstname, lastname, username, email from users left join houses using(house_id) where user_id = $1', [cookies.user])
+                res.status(200).json({ user: user.rows[0], auth: true })
+            } else {
+                res.status(401).json({ user: null, auth: false })
+            }
 
         } catch (error) {
             console.error(error.message)
@@ -16,4 +19,4 @@ async function verify(req, res) {
     }
 }
 
-export default authorization(verify)
+export default verify
