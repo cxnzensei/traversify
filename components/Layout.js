dd8d4b831d74aadd7a1adb843c37f51c81b381ad
cxnzensei <nishitpatil3231@gmail.com>
2022-06-23 08:22:47 +0530

    removed passing props to Sidebar

--- components/Layout.js
+++ components/Layout.js
@@ -1,21 +1,10 @@
 import Sidebar from "./Sidebar"
 import { useRouter } from "next/router"
-import { useEffect, useState } from "react"
+import { useState, useEffect } from "react"
 import checkAuth from "../utils/checkAuth"
 
 const Layout = ({ children }) => {
     const path = useRouter().pathname
-    const [user, setUser] = useState(null)
-
-    useEffect(() => {
-        const Auth = async () => {
-            const res = await checkAuth()
-            if(res?.auth) {
-                setUser(res?.user)
-            }
-        }
-        Auth()
-    }, [])
 
     if(path == '/login' || path == '/signup') {
         return (
@@ -24,7 +13,7 @@ const Layout = ({ children }) => {
     } else {
         return (
             <div className="flex">
-                <Sidebar user={user} />
+                <Sidebar />
                 {children}
             </div>
         )
